f2efdb05fbe6b9c0ab9fc214f469078518cc2a9d
felas <felafohim@gmail.com>
2024-06-28 18:35:24 -0400

    added UI to RPS and testing out branches on git

--- RPS.js
+++ RPS.js
@@ -3,94 +3,96 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
 //console.log(`${randomNumber} is the randomNumber variable`);
     let computerChoice = Values.at(randomNumber);
-    console.log(`${computerChoice} is what the computer chose.`);
+    console.log(`Computer throws ${computerChoice}.`);
     return computerChoice;
 }
 let w,x,y,z;
-
-function getHumanChoice(){
-    let humanChoice = prompt("What will you be throwing?: ");
-    console.log(`${humanChoice} is what you will throw.`);
-    return humanChoice;
-}
+let button1 = document.querySelector(".B1");
+let button2 = document.querySelector(".B2");
+let button3 = document.querySelector(".B3");
+let GameResult = document.querySelector("#Results")
+let Scoring = document.querySelector("#Score")
+let FinalScore  = document.querySelector("#FinalWin")
 
 let humanScore = 0;
 let computerScore = 0;
-
+let gameComment;
 function playRound(computerChoice, humanChoice){
     let Winner = ""
     Playee = ['Computer', 'Human', 'Niether'];
     if(computerChoice == humanChoice || humanChoice == ""){
         Winner = Playee[2]
-        
+        gameComment = (`The Same Throw: No Winner.`)
     }
     else if(computerChoice == 'Rock' && humanChoice == 'Scissors'){
         Winner = Playee[0]
         computerScore += 1
-        console.log("Rock breaks Scissors")
+        gameComment = (`Rock breaks Scissors: ${Winner} Wins this round!`)
     }
     else if(computerChoice == 'Paper' && humanChoice == 'Rock'){
         Winner = Playee[0]
         computerScore += 1
-        console.log("Paper covers Rock")
+        gameComment = (`Paper covers Rock: ${Winner} Wins this round!`)
     }
     else if(computerChoice == 'Scissors' && humanChoice == 'Rock'){
         Winner = Playee[1]
         humanScore += 1
-        console.log("Rock breaks Scissors")
+        gameComment = (`Rock breaks Scissors: ${Winner} Wins this round!`)
     }
     else if(computerChoice == 'Rock' && humanChoice == 'Paper'){
         Winner = Playee[1]
         humanScore += 1
-        console.log("Paper covers Rock")
+        gameComment = (`Paper covers Rock: ${Winner} Wins this round!`)
     }
     else if(computerChoice == 'Scissors' && humanChoice == 'Paper'){
         Winner = Playee[0]
         computerScore += 1
-        console.log("Scissors cuts Paper")
+        gameComment = (`Scissors cuts Paper: ${Winner} Wins this round!`)
     }
     else if(computerChoice == 'Paper' && humanChoice == 'Scissors'){
         Winner = Playee[1]
         humanScore += 1
-        console.log("Scissors cuts paper")
+        gameComment =(`Scissors cuts paper: ${Winner} Wins this round!`)
     }
-    console.log(`${Winner} is the Winner`)
-    return Winner
+    GameResult.textContent = gameComment
 }
-function callScore(){
-    console.log(`The score is Human:${humanScore} to Computer:${computerScore}`)
-}
+let GameWinner;
 
-
-function playGame(){
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
-    z = playRound(computerSelection, humanSelection)
-    roundOneScore = callScore()
-    const humanSelection2 = getHumanChoice()
-    const computerSelection2 = getComputerChoice()
-    z = playRound(computerSelection2, humanSelection2)
-    roundOneScore = callScore()
-    const humanSelection3 = getHumanChoice()
-    const computerSelection3 = getComputerChoice()
-    z = playRound(computerSelection3, humanSelection3)
-    roundOneScore = callScore()
-    const humanSelection4 = getHumanChoice()
-    const computerSelection4 = getComputerChoice()
-    z = playRound(computerSelection4, humanSelection4)
-    roundOneScore = callScore()
-    const humanSelection5 = getHumanChoice()
-    const computerSelection5 = getComputerChoice()
-    z = playRound(computerSelection5, humanSelection5)
-    roundOneScore = callScore()
-    let GameWinner;
+function updateScore(){
+    Scoring.textContent = `Human: ${humanScore} Computer: ${computerScore}`
     if (humanScore > computerScore){
         GameWinner = "Human";
     }
-    else if (computerScore > humanScore){
-        GameWinner = "Computer ";
+    if (computerScore > humanScore){
+        GameWinner = "Computer";
+    } 
+    if(computerScore == 5){
+        console.log("COMPUTER WINS")
+        FinalScore.textContent = "COMPUTER IS WINNER!"
     }
-    console.log(`Overall, Winner is ${GameWinner}`);
+    if(humanScore == 5){
+        console.log("HUMAN WINS!")
+        FinalScore.textContent = "HUMAN IS WINNER!"
+    }
 
-}
-x = playGame()
+};
+
+///Between playgame and playround functionality 
+button1.addEventListener('click', () => {
+    console.log('You threw Paper')
+    playRound(getComputerChoice(),"Paper");
+    updateScore()
+});
+
+button2.addEventListener('click', () => {
+    console.log('You threw Rock')
+    playRound(getComputerChoice(),"Rock");
+    updateScore()
+});
+
+button3.addEventListener('click', () => {
+    console.log('You threw Scissors')
+    playRound(getComputerChoice(), "Scissors");
+    updateScore()
+
+});
